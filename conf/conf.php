e8a98a0c038dde276f718ccbf88161d3e625a0d1
Greenou <greenou@gmail.com>
2020-08-25 10:13:46 +0200

    Ignorer
    Ignorer dossier conf

--- conf/conf.php
+++ conf/conf.php
@@ -2,18 +2,17 @@
 	// LOCAL
 	if($_SERVER['SERVER_ADDR']=="127.0.0.1"){
 		$host = "localhost";
-    	$user = "";
-    	$passwd = "";
-    	$db = ""; // A MODIFIER
+    	$user = "zarctus";
+    	$passwd = "fs73aw65";
+    	$db = "global";
 	}else{
 	// DISTANT
 		$host = "localhost:3307";
-        $user = "";
-        $passwd = "";
-        $db = ""; // A MODIFER
+        $user = "root";
+        $passwd = "fs73aw65";
+        $db = "global";
 	}
 
 	$pdo = new PDO('mysql:host='.$host.';dbname='.$db, $user, $passwd);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-
