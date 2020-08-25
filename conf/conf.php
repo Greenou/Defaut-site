d981ce7dfcdb7cdf37557ea6c8d762e1cd3340f0
Greenou <greenou@gmail.com>
2020-08-25 10:31:15 +0200

    Reset

---
+++ conf/conf.php
@@ -0,0 +1,18 @@
+<?php 
+	// LOCAL
+	if($_SERVER['SERVER_ADDR']=="127.0.0.1"){
+		$host = "localhost";
+    	$user = "";
+    	$passwd = "";
+    	$db = "";
+	}else{
+	// DISTANT
+		$host = "localhost:3307";
+        $user = "";
+        $passwd = "";
+        $db = "";
+	}
+
+	$pdo = new PDO('mysql:host='.$host.';dbname='.$db, $user, $passwd);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+<!-- FOOTER -->
+	    <script src="js/jquery-3.5.1.js"></script>
+	    <script src="js/popper.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
